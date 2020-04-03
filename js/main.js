a82ba33688f6f29cfc5bf030941291dfbc7af5e5
DenisRebenock <denmaloyreb@mail.ru>
2020-04-03 17:30:41 +0300

    refactor: add main menu

--- js/main.js
+++ js/main.js
@@ -4,6 +4,14 @@ $(function(){
         fade: true,
         autoplay: 100,
         dots: true
-    }); 
+    });
+    
+    $('.header-btn').on('click', function(){
+        $('.menu').addClass('active');
+    })
+
+    $('.close-btn').on('click', function() {
+        $('.menu').removeClass('active');
+    })
 
 });
